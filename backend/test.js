9a39bd222b368e88850b054562b32b04cf39f659
mahyarelectronic <smarabi121@gmail.com>
2025-07-29 19:48:43 +0330

    Update test.js

--- backend/test.js
+++ backend/test.js
@@ -1,1 +1,5 @@
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://smarabi121:eNpbpOS4s74KDDMu@cluster0.tvgtiiv.mongodb.net/simple-crm?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
