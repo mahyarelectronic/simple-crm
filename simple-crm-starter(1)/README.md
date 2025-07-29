0715820c5ff048ed9f2365941a6683ecd90ab74a
mahyarelectronic <smarabi121@gmail.com>
2025-07-29 15:02:24 +0330

    Delete simple-crm-starter(1) directory

--- simple-crm-starter(1)/README.md
+++
@@ -1,3 +0,0 @@
-# Simple CRM
-
-A simple customer management system with login and follow-up tracking.
--- simple-crm-starter(1)/backend/routes/customer.js
+++
@@ -1,9 +0,0 @@
-// Placeholder for customer route (will be filled later)
-const express = require('express');
-const router = express.Router();
-
-router.post('/', (req, res) => {
-  res.send('Add customer');
-});
-
-module.exports = router;
